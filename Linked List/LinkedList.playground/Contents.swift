7cdf43ea88e8eb4ccbd203ffb19784943de9ca50
Mukesh Thawani <mukesh9039@gmail.com>
2016-07-28 02:01:56 +0530

    Added first and last node property

--- Linked List/LinkedList.playground/Contents.swift
+++ Linked List/LinkedList.playground/Contents.swift
@@ -4,8 +4,8 @@ import Cocoa
 
 public class Node<T> {
     
-    var data:T
-    var next:Node?
+    var data: T
+    var next: Node?
     
     init(data:T) {
         self.data = data
@@ -15,27 +15,52 @@ public class Node<T> {
 
 public class LinkedList<T> {
     
-    var head:Node<T>?
+    var head: Node<T>?
     
     init() {
         self.head = nil
     }
-    
-    func printList() {
-        var currentNode = head
-        while (currentNode != nil) {
-            print((currentNode?.data)!,"\n")
-            currentNode = currentNode?.next
+   
+   
+   /// This prints all elements of a linked list.
+   public func printList() {
+    if var node = head {
+        print(node.data)
+        while node.next != nil {
+            node = node.next!
+            print(node.data)
         }
+    }
     }
     
     
-    func push(data:T) {
-        let newNode = Node(data: data)
+   /// Inserts a node at the front of the linked list.
+   public func push(data: T) {
+    let newNode = Node(data: data)
         
         newNode.next = self.head
         
         self.head = newNode
+    }
+    
+    
+    /// Returns first node.
+    public var first: Node<T> {
+        get {
+            return head!
+        }
+    }
+    
+    
+    /// Returns last node.
+    public var last: Node<T> {
+        get {
+            var currentNode = head
+            while (currentNode != nil && currentNode?.next != nil) {
+                currentNode = currentNode?.next
+            }
+            return currentNode!
+        }
     }
 }
 
@@ -48,5 +73,11 @@ ll.head = first
 ll.head?.next = second
 second.next = third
 
-ll.head?.data
-ll.printList()
+ll.head?.data   // 1
+
+ll.printList()  // 1
+                // 2
+                // 3
+
+ll.first.data // 1
+ll.last.data  // 3
