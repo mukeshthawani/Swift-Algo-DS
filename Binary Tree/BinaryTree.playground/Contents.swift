9c0fc96906795514cdbeebf2bc03b58bfeb2ce47
Mukesh Thawani <mukesh9039@gmail.com>
2016-08-25 23:33:25 +0530

    add height method in binary tree

--- Binary Tree/BinaryTree.playground/Contents.swift
+++ Binary Tree/BinaryTree.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import Cocoa
 
-class Node<T> {
+public class Node<T> {
   var left: Node<T>? = nil
   var right: Node<T>? = nil
   var data: T
@@ -12,3 +12,21 @@ class Node<T> {
   }
 }
 
+public class BinaryTree<T> where T: Comparable {
+  
+  public func height(node: Node<T>?) -> Int {
+    guard let node = node else {
+      return 0
+    }
+    return (1 + max(height(node: node.left), height(node: node.right)))
+  }
+}
+
+let root = Node<Int>(data: 1)
+root.left = Node<Int>(data: 2)
+root.right = Node<Int>(data: 3)
+root.left?.left = Node<Int>(data: 4)
+root.left?.left?.left = Node<Int>(data: 5)
+
+let binaryTree = BinaryTree<Int>()
+print(binaryTree.height(node: root))                   // 4
