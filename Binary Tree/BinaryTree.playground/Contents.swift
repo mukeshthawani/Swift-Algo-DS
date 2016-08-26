7c1c28fa2c690821aa7bdef3c5c2c59652de0951
Mukesh Thawani <mukesh9039@gmail.com>
2016-08-26 23:08:37 +0530

    Add diameter calculation for binary tree

--- Binary Tree/BinaryTree.playground/Contents.swift
+++ Binary Tree/BinaryTree.playground/Contents.swift
@@ -20,13 +20,27 @@ public class BinaryTree<T> where T: Comparable {
     }
     return (1 + max(height(node: node.left), height(node: node.right)))
   }
+  
+  public func diameter(node: Node<T>?) -> Int {
+    guard let node = node else {
+      return 0
+    }
+    let leftHeight = height(node: node.left)
+    let rightHeight = height(node: node.right)
+    
+    return max(diameter(node: node.left), diameter(node: node.right), (1 + leftHeight + rightHeight))
+  }
 }
 
 let root = Node<Int>(data: 1)
 root.left = Node<Int>(data: 2)
 root.right = Node<Int>(data: 3)
+root.left?.right = Node<Int>(data: 7)
+root.left?.right?.right = Node<Int>(data: 8)
+root.left?.right?.right?.right = Node<Int>(data: 9)
 root.left?.left = Node<Int>(data: 4)
 root.left?.left?.left = Node<Int>(data: 5)
 
 let binaryTree = BinaryTree<Int>()
-print(binaryTree.height(node: root))                   // 4
+print(binaryTree.height(node: root))                   // 5
+print(binaryTree.diameter(node: root))                 // 6
