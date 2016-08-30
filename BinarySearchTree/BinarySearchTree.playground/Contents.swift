29c7ccf120ac98fd718444b2334ff46474745806
Mukesh Thawani <mukesh9039@gmail.com>
2016-08-30 23:59:25 +0530

    Add search in BST

--- BinarySearchTree/BinarySearchTree.playground/Contents.swift
+++ BinarySearchTree/BinarySearchTree.playground/Contents.swift
@@ -2,12 +2,40 @@
 
 import Cocoa
 
-public class Node<T> {
-  var left: Node?
-  var right: Node?
+public class BinarySearchTree<T: Comparable> {
+  
+  var parent: BinarySearchTree?
+  var left: BinarySearchTree?
+  var right: BinarySearchTree?
   var data: T
   
   init(data: T) {
     self.data = data
   }
 }
+
+extension BinarySearchTree {
+  
+  public func search(key: T) -> BinarySearchTree? {
+    var node: BinarySearchTree? = self
+    while case let n? = node {
+      if key < n.data {
+        node = n.left
+      }
+      else if key > n.data {
+        node = n.right
+      }
+      else {
+        return node
+      }
+    }
+    return nil
+  }
+}
+
+let binTree = BinarySearchTree<Int>(data: 3)
+binTree.left?.data = 2
+binTree.left?.left?.data = 1
+binTree.right?.data = 4
+binTree.right?.right?.data = 5
+print(binTree.search(key: 1))
